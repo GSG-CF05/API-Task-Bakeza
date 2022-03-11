ca6ee365325c85013c3838ff06c9fd3c3e5fb12e
Bakeza <dbakeza2002@gmail.com>
2022-03-11 20:26:08 +0200

    Adding Js Code relate #7

---
+++ script.js
@@ -0,0 +1,79 @@
+let body = document.querySelector("body")
+let mainDiv = document.querySelector("#root")
+mainDiv.setAttribute('class', 'container')
+body.appendChild(mainDiv);
+fetch("http://hp-api.herokuapp.com/api/characters")
+    .then((res) => {
+        return res.json()
+    }).then((data) => {
+        data.forEach((ele) => {
+            // ! Variables
+            // let imageUrl = "image_link";
+
+            // ! Create Elements
+            let card = document.createElement("div")
+            let img = document.createElement("img")
+            let charName = document.createElement("h1")
+            let realName = document.createElement("h2")
+            let gender = document.createElement("p")
+            let dob = document.createElement("p")
+            let wizard = document.createElement("p")
+            let species = document.createElement("p")
+            let house = document.createElement("p")
+
+
+            card.setAttribute('class', 'card')
+            mainDiv.appendChild(card)
+
+            // ! Add data to the elements
+            if (ele.image != "") {
+                img.src = ele.image;
+                card.appendChild(img)
+
+            }
+            if (ele.name != "") {
+                charName.textContent = ele.name
+                card.appendChild(charName)
+
+            }
+            if (ele.actor != "") {
+                realName.textContent = ele.actor
+                card.appendChild(realName)
+
+            }
+            if (ele.gender != "") {
+                gender.textContent = "Gender : " + ele.gender
+                card.appendChild(gender)
+
+            }
+            if (ele.dateOfBirth != "") {
+                dob.textContent = "Date Of Birth : " + ele.dateOfBirth;
+                card.appendChild(dob)
+
+            } else if (ele.yearOfBirth != "") {
+                dob.textContent = "Year Of Birth : " + ele.yearOfBirth;
+                card.appendChild(dob)
+
+            }
+            if (ele.wizard != "") {
+                if (ele.wizard == true) {}
+                wizard.textContent = "Wizard : Yes";
+                card.appendChild(wizard)
+
+            }
+            if (ele.species != "") {
+                species.textContent = "Species : " + ele.species
+                card.appendChild(species)
+
+            }
+            if (ele.house != "") {
+                house.textContent = "House : " + ele.house;
+                card.appendChild(house)
+
+            }
+            //! Append Elements to HTML 
+
+
+        });
+
+    }).catch(Error)
